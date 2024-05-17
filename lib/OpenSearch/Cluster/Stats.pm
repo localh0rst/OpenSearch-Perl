5430ce913a8665db0db6160108292f27dd895ce4
localh0rst <git@fail.ninja>
2024-05-17 12:34:37 +0200

    Updated documentation

--- lib/OpenSearch/Cluster/Stats.pm
+++ lib/OpenSearch/Cluster/Stats.pm
@@ -6,20 +6,19 @@ use Moose;
 use Data::Dumper;
 
 with 'OpenSearch::Parameters::ClusterStats';
+
 #with 'OpenSearch::Helper';
 
 # Base singleton
-has 'base' => (is => 'rw', isa => 'OpenSearch::Base', lazy => 1, default => sub {OpenSearch::Base->instance});
+has 'base' => ( is => 'rw', isa => 'OpenSearch::Base', lazy => 1, default => sub { OpenSearch::Base->instance } );
 
-sub stats_p($self) {
-  return( $self->base->_get(
-    ['_cluster', 'stats', ($self->nodes ? ('nodes', $self->nodes->to_string) : ())]
-  ));
+sub get_p($self) {
+  return ( $self->base->_get( [ '_cluster', 'stats', ( $self->nodes ? ( 'nodes', $self->nodes->to_string ) : () ) ] ) );
 }
 
-sub stats($self) {
+sub get($self) {
   my ($res);
-  $self->stats_p->then(sub {$res = shift; })->wait;
+  $self->stats_p->then( sub { $res = shift; } )->wait;
   return $res;
 }
 
