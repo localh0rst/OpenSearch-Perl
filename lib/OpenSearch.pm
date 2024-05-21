9f89ecd5ed6f3d260dc28cda331920a702ea10d7
localh0rst <git@fail.ninja>
2024-05-21 10:44:40 +0200

    Make API mor consistent

--- lib/OpenSearch.pm
+++ lib/OpenSearch.pm
@@ -40,19 +40,14 @@ sub BUILD( $self, $args ) {
   ) );
 }
 
-#Search
+#Search Endpoint
 sub search { shift; return ( OpenSearch::Search->new(@_) ); }
 
 # Cluster
-sub cluster            { shift; return ( OpenSearch::Cluster->new(@_) ); }
-sub cluster_health     { shift; return ( OpenSearch::Cluster::Health->new(@_) ); }
-sub cluster_stats      { shift; return ( OpenSearch::Cluster::Stats->new(@_) ); }
-sub cluster_allocation { shift; return ( OpenSearch::Cluster::Allocation->new(@_) ); }
-sub cluster_settings   { shift; return ( OpenSearch::Cluster::Settings->new(@_) ); }
+sub cluster { shift; return ( OpenSearch::Cluster->new(@_) ); }
 
 # Remote
-sub remote      { shift; return ( OpenSearch::Remote->new(@_) ); }
-sub remote_info { shift; return ( OpenSearch::Remote::Info->new(@_) ); }
+sub remote { shift; return ( OpenSearch::Remote->new(@_) ); }
 
 1;
 
@@ -77,30 +72,6 @@ C<OpenSearch> - A Perl client for OpenSearch (https://opensearch.org/)
       secure => 0,
       allow_insecure => 1,
     );
-
-    my $s = $self->search(
-      index => 'my_index',
-      query => {
-        bool => {
-          must => [ { range => { '@timestamp' => { gte => 'now-1d' } } } ],
-        }
-      }
-    );
-
-    # Blocking
-    my $response = $s->execute; 
-    # Non Blocking - Returns a Mojo::Promise;
-    my $promise = $s->execute_p->then(...)->catch(...);
-
-    # OR you can do it like this:
-    my $response = $s->search
-      ->index('my_index')
-      ->query({ 
-        bool => { 
-          must => [ { range => { '@timestamp' => { gte => 'now-1d' } } } ] 
-        } 
-      }
-    )->execute;
 
 =head1 DESCRIPTION
 
@@ -196,41 +167,11 @@ returns a new OpenSearch::Cluster object
 
   my $cluster = $opensearch->cluster;
 
-=head2 cluster_allocation
-
-returns a new OpenSearch::Cluster::Allocation object
-
-  my $cluster = $opensearch->cluster_allocation;
-
-=head2 cluster_health
-
-returns a new OpenSearch::Cluster::Health object
-
-  my $cluster = $opensearch->cluster_health;
-
-=head2 cluster_settings
-
-returns a new OpenSearch::Cluster::Settings object
-
-  my $cluster = $opensearch->cluster_settings;
-
-=head2 cluster_stats
-
-returns a new OpenSearch::Cluster::Stats object
-
-  my $cluster = $opensearch->cluster_stats;
-
 =head2 remote
 
 returns a new OpenSearch::Remote object
 
   my $remote = $opensearch->remote;
-
-=head2 remote_info
-
-returns a new OpenSearch::Remote::Info object
-
-  my $remote = $opensearch->remote_info;
 
 =head2 search
 
--- lib/OpenSearch/Search.pm
+++ lib/OpenSearch/Search.pm
@@ -1,92 +1,84 @@
 package OpenSearch::Search;
 use strict;
 use warnings;
-use feature qw(signatures);
 use Moose;
-use OpenSearch::Scroll;
+use feature qw(signatures);
 use Data::Dumper;
 
-with 'OpenSearch::Parameters::Search';
+use OpenSearch::Search::Search;
+use OpenSearch::Search::Scroll;
 
-#with 'OpenSearch::Helper';
-
-# Base singleton
-has 'base' => (
-  is       => 'rw',
-  isa      => 'OpenSearch::Base',
-  required => 0,
-  lazy     => 1,
-  default  => sub { OpenSearch::Base->instance; }
-);
-
-# Scroll parameters
-has 'scroll_id' => ( is => 'rw', isa => 'Str' );
-
-sub execute_p($self) {
-  my $params = {
-    optional => {
-      url => [
-        qw/
-          max_concurrent_shard_requests stored_fields ignore_throttled allow_no_indices q request_cache analyze_wildcard suggest_text
-          rest_total_hits_as_int routing track_total_hits cancel_after_time_interval _source_includes pre_filter_shard_size suggest_field
-          preference suggest_size default_operator suggest_mode allow_partial_search_results search_type expand_wildcards typed_keys
-          ignore_unavailable df batched_reduce_size analyzer _source_excludes track_scores lenient ccs_minimize_roundtrips scroll
-          /
-      ],
-      body => [
-        qw/
-          seq_no_primary_term version explain stats from min_score size terminate_after docvalue_fields indices_boost fields query _source timeout size sort search_after
-          /
-      ]
-    }
-  };
-
-  return ( $self->base->_get( $self, [ ( $self->index // () ), '_search' ], $params ) );
-}
-
-sub execute($self) {
-  my ($res);
-  $self->execute_p->then( sub { $res = shift; } )->wait;
-  return $res;
-}
-
-sub search_scroll_p($self) {
-  if ( $self->scroll_id ) {
-    $self->base->ua->get_p(
-      $self->base->url( [ '_search', 'scroll' ] ) => json => {
-        scroll    => $self->scroll // '10m',
-        scroll_id => $self->scroll_id,
-
-        # slice => {id => 0, max => 10} These belong only in the initial searech request. not here
-      }
-    )->then( sub($tx) {
-      $self->scroll_id( $tx->result->json->{_scroll_id} ) if $tx->result->json->{_scroll_id};
-      return ( $self->base->response($tx) );
-    } )->catch( sub($error) {
-      die( $error . "\n" );
-    } );
-  } else {
-
-    # If not already in "scroll" mode, use a normal search including scroll
-    $self->search_p;
-  }
-}
-
-sub search_scroll($self) {
-  my ($res);
-  $self->search_scroll_p->then( sub { $res = shift; } )->wait;
-  return $res;
-}
-
-sub scroll_delete_p($self) {
-  return ( OpenSearch::Scroll->new->delete_p( $self->scroll_id ) );
-}
-
-sub scroll_delete($self) {
-  my ($res);
-  my $scroll = OpenSearch::Scroll->new;
-  $scroll->delete_p( $self->scroll_id )->then( sub { $res = shift } )->wait;
-  return ($res);
-}
+sub search { shift; return ( OpenSearch::Search::Search->new(@_) ); }
+sub scroll { shift; return ( OpenSearch::Search::Scroll->new(@_) ); }
 
 1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+C<OpenSearch::Search> - OpenSearch Search API
+
+=head1 SYNOPSIS
+
+    use strict;
+    use warnings;
+    use OpenSearch;
+
+    my $opensearch = OpenSearch->new(
+      user => 'admin',
+      pass => 'admin',
+      hosts => ['http://localhost:9200'],
+      secure => 0,
+      allow_insecure => 1,
+    );
+
+    my $search_api = $opensearch->search;
+
+=head1 DESCRIPTION
+
+This is the Module for the OpenSearch Search API. The following Endpoints
+are currently supported:
+
+=over 4
+
+=item * Search [L<OpenSearch::Search::Search>] (GET /<target-index>/_search)
+
+=item * Scroll [L<OpenSearch::Search::Scroll>] (GET /_search/scroll=10m)
+
+=back
+
+=head1 METHODS
+
+=head2 search
+
+returns a new OpenSearch::Search::Search object. 
+
+  my $search_api = $opensearch->search;
+  my $res = $search_api->search(
+    index => 'my_index',
+    query => {
+      match_all => {}
+    }
+  )->execute;
+
+=head2 health
+
+returns a new OpenSearch::Search::Scroll object
+
+  my $health = $opensearch->search->scroll;
+
+=head1 LICENSE
+
+Copyright (C) localh0rst.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+localh0rst E<lt>git@fail.ninjaE<gt>
+
+=cut
+
