8d3ab3fc82639acd2d9c7ed2c42e2417dc74eb13
localh0rst <git@fail.ninja>
2024-05-24 15:45:09 +0200

    Added documentation (a little)

--- lib/OpenSearch.pm
+++ lib/OpenSearch.pm
@@ -12,6 +12,8 @@ use OpenSearch::Search;
 use OpenSearch::Index;
 use OpenSearch::Document;
 
+our $VERSION = '0.90';
+
 has 'base' => (
   is      => 'rw',
   isa     => 'OpenSearch::Base',
@@ -52,5 +54,150 @@ sub index($self) {
 sub document($self) {
   return ( OpenSearch::Document->new );
 }
+
 1;
+
+__END__
+
+=head1 NAME
+
+C<OpenSearch> - Unofficial Perl client for OpenSearch (https://opensearch.org/)
+
+=head1 SYNOPSIS
+
+  use OpenSearch;
+
+  my $opensearch = OpenSearch->new(
+    user            => 'admin',
+    pass            => 'password',
+    hosts           => [ 'http://localhost:9200' ],
+    secure          => 0,
+    allow_insecure  => 1,
+    pool_count      => 10,
+    clear_attrs     => 0,
+    async           => 0,
+    max_connections => 10,
+  );
+
+  my $cluster = $opensearch->cluster;
+  my $remote  = $opensearch->remote;
+  my $search  = $opensearch->search;
+  my $index   = $opensearch->index;
+  my $document = $opensearch->document;
+
+=head1 DESCRIPTION
+
+C<OpenSearch> is an unofficial Perl client for OpenSearch (https://opensearch.org/).
+Currently it only supports a subset of the OpenSearch API. However, it is a work in 
+progress and more features will be added in the future. Currently, the following
+endpoints are (partially) supported:
+
+=over 4
+
+=item * Cluster
+
+=item * Remote
+
+=item * Search
+
+=item * Index
+
+=item * Document
+
+=back
+
+=head1 IMPORTANT
+
+This module is still in development and should not be used in production unless you
+are willing to accept the risks associated with using an incomplete and untested
+module. It heavily relies on L<Moose> and L<Mojo::UserAgent>. Due to the use of
+L<Moose>, startup time is slower than other modules. However, the use of L<Mojo::UserAgent>
+allows for asynchronous requests to be made to the OpenSearch server.
+
+CERTIFICATE AUTHENTICATION IS NOT YET TESTED! Feel free to test it and report back to me.
+
+=head1 METHODS
+
+=head2 new
+
+Creates a new instance of C<OpenSearch>.
+
+=head2 cluster
+
+Returns a new instance of C<OpenSearch::Cluster>.
+
+=head2 remote
+
+Returns a new instance of C<OpenSearch::Remote>.
+
+=head2 search
+
+Returns a new instance of C<OpenSearch::Search>.
+
+=head2 index
+
+Returns a new instance of C<OpenSearch::Index>.
+
+=head2 document
+
+Returns a new instance of C<OpenSearch::Document>.
+
+=head1 ATTRIBUTES
+
+=over 4
+
+=item * user
+
+The username to use when connecting to the OpenSearch server.
+
+=item * pass
+
+The password to use when connecting to the OpenSearch server.
+
+=item * hosts
+
+An array reference containing the hostnames of the OpenSearch server(s).
+
+=item * secure
+
+A boolean value indicating whether to use HTTPS when connecting to the OpenSearch server.
+
+=item * allow_insecure
+
+A boolean value indicating whether to allow insecure connections to the OpenSearch server.
+
+=item * pool_count
+
+The number of connections to pool when connecting to the OpenSearch server.
+
+=item * clear_attrs
+
+A boolean value indicating whether to clear the attributes of most objects.
+
+=item * async
+
+A boolean value indicating whether to use asynchronous requests when connecting to the OpenSearch server.
+This will return a L<Mojo::Promise> object instead of the actual response.
+
+=item * max_connections
+
+The maximum number of connections to allow when connecting to the OpenSearch server (see L<Mojo::UserAgent>).
+
+=item * ca_cert
+
+The path to the CA certificate to use when connecting to the OpenSearch server.
+
+=item * client_cert
+
+The path to the client certificate to use when connecting to the OpenSearch server.
+
+=item * client_key
+
+The path to the client key to use when connecting to the OpenSearch server.
+
+=back
+
+=head1 AUTHOR
+
+C<OpenSearch> was written by Sebastian Grenz, C<< <git at fail.ninja> >>
 
--- lib/OpenSearch/Cluster.pm
+++ lib/OpenSearch/Cluster.pm
@@ -31,3 +31,73 @@ sub allocation_explain( $self, @params ) {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+C<OpenSearch::Cluster> - Cluster API
+
+=head1 SYNOPSIS
+
+  use OpenSearch;
+  my $os = OpenSearch->new(
+    ...
+    async => 1
+  );
+
+  my $cluster = $os->cluster;
+
+  # When async is set, this returns a L<Mojo::Promise> object
+  # Otherwise, it returns a hashref with the JSON response. 
+  my $settings = $cluster->get_settings;
+
+  $cluster->put_settings(
+    persistent => {
+      'indices.recovery.max_bytes_per_sec' => '50mb'
+    },
+    transient => {
+      'cluster.routing.allocation.enable' => 'all'
+    }
+    timeout => '30s'
+  );
+
+  my $health = $cluster->health(...);
+
+=head1 DESCRIPTION
+
+This module provides an interface to the OpenSearch Cluster API.
+
+=head1 METHODS
+
+=head2 get_settings
+
+  $cluster->get_settings;
+
+=head2 put_settings
+
+  $cluster->put_settings(
+    persistent => {
+      'indices.recovery.max_bytes_per_sec' => '50mb'
+    },
+    transient => {
+      'cluster.routing.allocation.enable' => 'all'
+    }
+    timeout => '30s'
+  );
+
+=head2 health
+
+  $cluster->health(...);
+
+=head2 stats
+
+  $cluster->stats(...);
+
+=head2 allocation_explain
+
+  $cluster->allocation_explain(...);
+
+=head1 AUTHOR
+
+C<OpenSearch> was written by Sebastian Grenz, C<< <git at fail.ninja> >>
