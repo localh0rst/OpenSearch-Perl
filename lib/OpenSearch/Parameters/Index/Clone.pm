b2d2b2773e1bdc03ede9678bd2ce9ceafaee854a
localh0rst <git@fail.ninja>
2024-05-27 15:34:40 +0200

    Added missing settings and aliases attributes

--- lib/OpenSearch/Parameters/Index/Clone.pm
+++ lib/OpenSearch/Parameters/Index/Clone.pm
@@ -23,6 +23,28 @@ has 'target' => (
   }
 );
 
+has 'settings' => (
+  is          => 'rw',
+  isa         => 'HashRef',
+  metaclass   => 'MooseX::MetaDescription::Meta::Attribute',
+  description => {
+    encode_func => 'as_is',
+    type        => 'body',
+    required    => 0,
+  }
+);
+
+has 'aliases' => (
+  is          => 'rw',
+  isa         => 'HashRef',
+  metaclass   => 'MooseX::MetaDescription::Meta::Attribute',
+  description => {
+    encode_func => 'as_is',
+    type        => 'body',
+    required    => 0,
+  }
+);
+
 has 'wait_for_active_shards' => (
   is          => 'rw',
   isa         => 'Str',
@@ -79,8 +101,8 @@ has 'task_execution_timeout' => (
 );
 
 around [
-  qw/index target wait_for_active_shards cluster_manager_timeout timeout wait_for_completion task_execution_timeout/]
-  => sub {
+  qw/index target settings aliases wait_for_active_shards cluster_manager_timeout timeout wait_for_completion task_execution_timeout/
+] => sub {
   my $orig = shift;
   my $self = shift;
 
@@ -89,6 +111,6 @@ around [
     return ($self);
   }
   return ( $self->$orig );
-  };
+};
 
 1;
