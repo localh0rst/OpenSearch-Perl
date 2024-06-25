19f901ba4eea61eb161fe9662b99b66769137396
localh0rst <git@fail.ninja>
2024-06-25 10:53:18 +0200

    Removed pointless default method and lazy from the base attribute.

--- lib/OpenSearch.pm
+++ lib/OpenSearch.pm
@@ -16,10 +16,11 @@ use OpenSearch::Document;
 our $VERSION = '0.92';
 
 has 'base' => (
-  is      => 'rw',
-  isa     => 'OpenSearch::Base',
-  lazy    => 1,
-  default => sub { OpenSearch::Base->initialize; }
+  is  => 'rw',
+  isa => 'OpenSearch::Base',
+
+  #lazy => 1,
+  #default => sub { OpenSearch::Base->initialize; }
 );
 
 sub BUILD( $self, $args ) {
