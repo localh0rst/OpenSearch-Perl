26b82d59ce0abb861daf83a7174b716ad89cb558
localh0rst <git@fail.ninja>
2024-06-27 16:36:04 +0200

    Added Security SSLInfo and PermissionsInfo

--- lib/OpenSearch/Security.pm
+++ lib/OpenSearch/Security.pm
@@ -5,6 +5,8 @@ use Moo;
 use Types::Standard qw(InstanceOf);
 use Data::Dumper;
 use OpenSearch::Security::Whoami;
+use OpenSearch::Security::SSLInfo;
+use OpenSearch::Security::PermissionsInfo;
 use feature qw(signatures);
 no warnings qw(experimental::signatures);
 
@@ -16,6 +18,14 @@ has '_base' => (
 
 sub whoami( $self, @params ) {
   return ( OpenSearch::Security::Whoami->new( @params, _base => $self->_base )->execute );
+}
+
+sub sslinfo( $self, @params ) {
+  return ( OpenSearch::Security::SSLInfo->new( @params, _base => $self->_base )->execute );
+}
+
+sub permissions_info( $self, @params ) {
+  return ( OpenSearch::Security::PermissionsInfo->new( @params, _base => $self->_base )->execute );
 }
 
 1;
