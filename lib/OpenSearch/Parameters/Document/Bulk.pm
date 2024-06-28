389f19563edf889e01f5a76c8a17270e61ee6e2c
localh0rst <git@fail.ninja>
2024-06-28 13:35:42 +0200

    Changed _body to docs

--- lib/OpenSearch/Parameters/Document/Bulk.pm
+++ lib/OpenSearch/Parameters/Document/Bulk.pm
@@ -1,56 +1,56 @@
 package OpenSearch::Parameters::Document::Bulk;
 use strict;
 use warnings;
-use feature qw(state);
+use feature         qw(state);
 use Types::Standard qw(Str ArrayRef Bool Enum);
 use Moo::Role;
 
 with 'OpenSearch::Parameters';
 
 has 'index' => (
-  is          => 'rw',
-  isa         => Str,
+  is  => 'rw',
+  isa => Str,
 );
 
-has '_body' => (
-  is          => 'rw',
-  isa         => ArrayRef,
-  required    => 1,
+has 'docs' => (
+  is       => 'rw',
+  isa      => ArrayRef,
+  required => 1,
 );
 
 has 'pipeline' => (
-  is          => 'rw',
-  isa         => Str,
+  is  => 'rw',
+  isa => Str,
 );
 
 has 'refresh' => (
-  is          => 'rw',
-  isa         => Enum[qw(true false wait_for)],
+  is  => 'rw',
+  isa => Enum [qw(true false wait_for)],
 );
 
 has 'require_alias' => (
-  is          => 'rw',
-  isa         => Bool,
+  is  => 'rw',
+  isa => Bool,
 );
 
 has 'routing' => (
-  is          => 'rw',
-  isa         => Str,
+  is  => 'rw',
+  isa => Str,
 );
 
 has 'timeout' => (
-  is          => 'rw',
-  isa         => Str,
+  is  => 'rw',
+  isa => Str,
 );
 
 has 'wait_for_active_shards' => (
-  is          => 'rw',
-  isa         => Str,
+  is  => 'rw',
+  isa => Str,
 );
 
 around [
   qw/
-    index pipeline refresh require_alias routing timeout wait_for_active_shards
+    index pipeline refresh require_alias routing timeout wait_for_active_shards docs
     /
 ] => sub {
   my $orig = shift;
@@ -69,7 +69,7 @@ sub api_spec {
       encode_func => 'as_is',
       type        => 'path',
     },
-    _body => {
+    docs => {
       encode_func => 'encode_bulk',
       type        => 'body',
       forced_body => 1,
