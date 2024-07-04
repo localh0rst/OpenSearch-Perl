96ac03b54b5d6dd9b0f4b8eabe72cc454bc1be73
localh0rst <git@fail.ninja>
2024-07-04 11:58:37 +0200

    Return OpenSearch::Response also on connection errors (or any error thrown by Mojo::UserAgent)

--- lib/OpenSearch/Response.pm
+++ lib/OpenSearch/Response.pm
@@ -3,10 +3,10 @@ use strict;
 use warnings;
 use Moo;
 use Types::Standard qw(Str Bool Int);
-use feature qw(signatures);
+use feature         qw(signatures);
 no warnings qw(experimental::signatures);
 
-has '_response' => ( is => 'rw', required => 1 );
+has '_response' => ( is => 'rw', required => 0 );
 has 'success'   => ( is => 'rw', isa      => Bool, required => 0 );
 has 'message'   => ( is => 'rw', isa      => Str,  required => 0 );
 has 'error'     => ( is => 'rw', required => 0 );
@@ -14,16 +14,17 @@ has 'code'      => ( is => 'rw', isa      => Int, required => 0 );
 has 'data'      => ( is => 'rw', required => 0 );
 
 sub BUILD( $self, @rest ) {
-  $self->success( $self->_response->code >= 200 && $self->_response->code < 300 );
+  if ( $self->_response ) {
+    $self->success( $self->_response->code >= 200 && $self->_response->code < 300 );
 
-  $self->code( $self->_response->code );
-  $self->message( $self->_response->message );
-  $self->data( $self->_response->json );
+    $self->code( $self->_response->code );
+    $self->message( $self->_response->message );
+    $self->data( $self->_response->json );
 
-  if ( !$self->success && ( $self->data && $self->data->{error} ) ) {
-    $self->error( $self->data->{error} );
+    if ( !$self->success && ( $self->data && $self->data->{error} ) ) {
+      $self->error( $self->data->{error} );
+    }
   }
-
 }
 
 1;
